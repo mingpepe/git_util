f3d45e40063a8652e4d6d8c1ffb475e3a7532bb0
mingpepe <mingpepemath@gamil.com>
2021-11-02 21:16:57 +0800

    Print colorful output

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mingpepe/git_util
 
 go 1.16
+
+require github.com/fatih/color v1.13.0 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
+	"github.com/fatih/color"
 	"github.com/mingpepe/git_util/util"
 )
+
+func printGitRepo(r util.GitRepo) {
+	msg := fmt.Sprintf("%-50s %-20s %-20s", r.Path, r.BranchName, r.State.String())
+	switch r.State {
+	case util.UPDATE_TO_DATE:
+		color.Green(msg)
+	case util.UN_PUSHED:
+		color.Yellow(msg)
+	case util.UN_COMMITED:
+		color.Red(msg)
+	case util.UN_STAGED:
+		color.Red(msg)
+	case util.NO_COMMITS_YET:
+		color.Red(msg)
+	default:
+		panic("unknown git state")
+	}
+}
 
 func main() {
 	var path = flag.String("p", ".", "For git repo path")
@@ -13,11 +33,8 @@ func main() {
 
 	if util.IsGitSupport() {
 		ret := util.Probe(*path)
-		for _, a := range ret {
-			log.Printf("Path : %s\n", a.Path)
-			log.Printf("Branch name : %s\n", a.BranchName)
-			log.Printf("Any untracked files : %v\n", a.AnyUntrackedFiles)
-			log.Printf("State : %v\n\n", a.State)
+		for _, r := range ret {
+			printGitRepo(r)
 		}
 	} else {
 		log.Print("Git seems not installed yet")
